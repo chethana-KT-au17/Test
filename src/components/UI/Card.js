e9ead48dbeb20aece21df8723aba298a97b08669
chethanakt3006@gmail.com <chethanakt3006@gmail.com>
2021-07-23 10:03:11 +0530

    firsrt commit

---
+++ src/components/UI/Card.js
@@ -0,0 +1,11 @@
+
+import classes from './Card.css';
+
+const Card = props => {
+    return <div className={classes.card}> 
+           {props.children}
+        </div>
+    
+};
+
+export default Card;
